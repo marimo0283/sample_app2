5d1ec40e272d7c2d93b36bd0dbec147982c840cb
marimo0283 <kugelschreiber1002@gmail.com>
2023-05-05 10:02:26 +0000

    [Add]投稿編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'lists/new'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   post 'lists' => 'lists#create'
   get '/top' => 'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
