d6bb1ff51fe9ccee96b8d2e0c82d6f4b03a6f51e
marimo0283 <kugelschreiber1002@gmail.com>
2023-05-23 08:43:56 +0000

    [Add]RSpec準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  post 'lists' => 'lists#create'
   get '/top' => 'homes#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists
 end
